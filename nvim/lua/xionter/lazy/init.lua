0f093576a4ff6c09a1dce19c9dfb36f39bc1a918
xionter <141619592+xionter@users.noreply.github.com>
2025-03-03 19:24:57 +0500

    Update init.lua

--- nvim/lua/xionter/lazy/init.lua
+++ nvim/lua/xionter/lazy/init.lua
@@ -1,9 +1,52 @@
 return {
     "nvim-lua/plenary.nvim",
-    "HealsCodes/vim-gas",
     "ThePrimeagen/vim-be-good",
-    { "morhetz/gruvbox", name = "gruvbox",
-    config = function()
-        vim.cmd("colorscheme gruvbox")
-    end},
+    {"SunnyTamang/pendulum.nvim",
+        config = function()
+            require"pendulum".setup({
+            })
+        end
+
+    },
+    {
+        'jesseleite/nvim-noirbuddy',
+        dependencies = {
+            { 'tjdevries/colorbuddy.nvim' }
+        },
+        lazy = false,
+        priority = 1000,
+        config = function()
+            vim.cmd("colorscheme noirbuddy")
+            require("noirbuddy").setup({
+                preset = 'crt-green',
+                colors = {
+                    background = '#0D120D',
+                    primary = '#8A9A5B',
+                    secondary = '#E9F5E9',
+                    noir_0 = '#C3D8A8',
+                    noir_1 = '#B5C89E',
+                    noir_2 = '#9AA69A',
+                    noir_3 = '#808B80',
+                    noir_4 = '#707970',
+                    noir_5 = '#616861',
+                    noir_6 = '#4F584F',
+                    noir_7 = '#3D463D',
+                    noir_8 = '#303630',
+                    noir_9 = '#282E28',
+                    diagnostic_error = '#800020',
+                    diagnostic_warning = '#800020',
+                    diagnostic_info = '#800020',
+                    diagnostic_hint = '#800020',
+                    diff_add = '#800020',
+                    diff_change = '#800020',
+                    diff_delete = '#800020', 
+                },
+        })
+        end,
+    },
+
+    --{"ntk148v/komau.vim", name = "komau",
+    --config = function()
+    --    vim.cmd("colorscheme komau")
+    --end},
 }
